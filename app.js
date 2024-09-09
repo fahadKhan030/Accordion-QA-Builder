9b06ee1ad869cdc14a33048e675fb349053820e2
fahadkhan030 <fahdkhn30@gmail.com>
2024-09-09 12:10:47 -0700

    Add close button to remove questions and improve UI responsiveness

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let question = document.getElementById("question");
 let ans = document.getElementById("ans");
 
 function createQuestionAnswer(questionText, answerText) {
+
   // Create container for the question and answer
   const container = document.createElement("div");
   container.classList.add(
@@ -10,20 +11,33 @@ function createQuestionAnswer(questionText, answerText) {
     "item-center",
     "justify-center",
     "w-[90%]",
-    "mb-4"
+    "mb-4",
+    
+
   );
+
+  const questionContainer = document.createElement('div');
+  questionContainer.classList.add ( "flex","items-center","justify-between", "bg-blue-500", "text-white","w-full","rounded-md",)
+
+
+  const closeImage = document.createElement('img');
+  closeImage.src = 'https://flaticons.net/icon.php?slug_category=mobile-application&slug_icon=close'
+  closeImage.style.height = '10px'
+  closeImage.classList.add('pr-2')
 
   // Create the question element
   const question = document.createElement("h2");
   question.classList.add(
     "text-xl",
     "text-white",
-    "w-[90%]",
+    // "w-[90%]",
     "font-semibold",
     "bg-blue-500",
     "rounded-md",
     "pl-2",
-    "cursor-pointer"
+    "cursor-pointer",
+    
+
   );
   question.textContent = questionText;
 
@@ -34,25 +48,33 @@ function createQuestionAnswer(questionText, answerText) {
     "text-lg",
     "text-white",
     "w-[90%]",
-    "bg-slate-400",
+    "bg-green-500",
     "rounded-tl-none",
     "rounded-tr-none",
     "pl-2",
-    "rounded-md"
+    "rounded-md",
+    "w-full"
   );
     
   const answerParagraph = document.createElement("p");
-  answerParagraph.classList.add("text-white");
+  answerParagraph.classList.add("text-white", "w-full");
   answerParagraph.textContent = answerText;
 
-  container.appendChild(question);
+
+  container.appendChild(questionContainer)
+  questionContainer.appendChild(question);
+  questionContainer.appendChild(closeImage)
   container.appendChild(answer);
   answer.appendChild(answerParagraph);
 
   // Add click event listener to toggle the answer visibility
-  question.addEventListener("click", function () {
+  questionContainer.addEventListener("click", function () {
     answer.classList.toggle("hidden");
-    question.classList.toggle("rounded-b-none");
+    questionContainer.classList.toggle("rounded-b-none");
+  });
+
+  closeImage.addEventListener("click", function (event) {
+    container.remove();
   });
 
   return container; // Return the created container
