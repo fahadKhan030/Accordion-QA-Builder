afe1d322cbd70e0b374d6dc4d3be54d56fe57304
fahad khan <fahdkhn30@gmail.com>
2024-09-10 04:56:52 -0700

    changes in UI

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let question = document.getElementById("question");
 let ans = document.getElementById("ans");
 
 function createQuestionAnswer(questionText, answerText) {
-
   // Create container for the question and answer
   const container = document.createElement("div");
   container.classList.add(
@@ -11,35 +10,52 @@ function createQuestionAnswer(questionText, answerText) {
     "item-center",
     "justify-center",
     "w-[90%]",
-    "mb-4",
-    
-
+    "mb-4"
   );
 
-  const questionContainer = document.createElement('div');
-  questionContainer.classList.add ( "flex","items-center","justify-between", "bg-blue-500", "text-white","w-full","rounded-md",)
-
-
-  const closeImage = document.createElement('img');
-  closeImage.src = 'https://flaticons.net/icon.php?slug_category=mobile-application&slug_icon=close'
-  closeImage.style.height = '10px'
-  closeImage.classList.add('pr-2')
+  const questionContainer = document.createElement("div");
+  questionContainer.classList.add(
+    "flex",
+    "items-center",
+    "justify-between",
+    "bg-blue-500",
+    "text-white",
+    "w-full",
+    "rounded-md",
+    "flex-row-reverse"
+  );
 
   // Create the question element
   const question = document.createElement("h2");
   question.classList.add(
     "text-xl",
     "text-white",
-    // "w-[90%]",
+    "w-[80%]",
     "font-semibold",
     "bg-blue-500",
     "rounded-md",
     "pl-2",
-    "cursor-pointer",
-    
-
+    "cursor-pointer"
   );
   question.textContent = questionText;
+
+  
+  //  create a Element for the edit and close
+  const closeEdit = document.createElement("div");
+  closeEdit.classList.add(
+    "flex",
+    "items-center",
+    "justify-between",
+    "pr-2",
+    "gap-2"
+  );
+
+  // elemtnt for close
+  const closeImage = document.createElement("img");
+  closeImage.src =
+    "https://flaticons.net/icon.php?slug_category=mobile-application&slug_icon=close";
+  closeImage.style.height = "10px";
+  closeImage.classList.add("cursor-pointer");
 
   // Create the answer element
   const answer = document.createElement("div");
@@ -55,20 +71,21 @@ function createQuestionAnswer(questionText, answerText) {
     "rounded-md",
     "w-full"
   );
-    
+
   const answerParagraph = document.createElement("p");
   answerParagraph.classList.add("text-white", "w-full");
   answerParagraph.textContent = answerText;
 
-
-  container.appendChild(questionContainer)
+  questionContainer.appendChild(closeEdit);
+  closeEdit.appendChild(closeImage);
+  container.appendChild(questionContainer);
   questionContainer.appendChild(question);
-  questionContainer.appendChild(closeImage)
+
   container.appendChild(answer);
   answer.appendChild(answerParagraph);
 
   // Add click event listener to toggle the answer visibility
-  questionContainer.addEventListener("click", function () {
+  question.addEventListener("click", function () {
     answer.classList.toggle("hidden");
     questionContainer.classList.toggle("rounded-b-none");
   });
